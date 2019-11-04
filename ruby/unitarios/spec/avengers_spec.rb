7970f4a8538ea68d12377f59424bd4d3ee82af6e
Rafael Ferreira <rafael.ferreira@before.com.br>
2019-11-04 13:49:13 -0300

    OK

--- ruby/unitarios/spec/avengers_spec.rb
+++ ruby/unitarios/spec/avengers_spec.rb
@@ -1,4 +1,5 @@
 class AvengersHeadQuarter
+
     attr_accessor :list
 
     def initialize
@@ -9,9 +10,9 @@ class AvengersHeadQuarter
         self.list.push(avenger)
     end
 end
-
+# TDD (DESENVOLVIMENTO GUIADO POR TESTES)
 describe AvengersHeadQuarter do
-    it 'Deve adicionar um parceiro' do
+    it 'Deve adicionar um vingador' do
         hq = AvengersHeadQuarter.new
 
         hq.put('Spiderman')
@@ -25,8 +26,37 @@ describe AvengersHeadQuarter do
         hq.put('hulk')
         hq.put('thor')
 
+        res = hq.list.size > 0
+
         expect(hq.list).to include 'hulk'
         expect(hq.list.size).to be > 0
-        
+        expect(res).to be true
+    end
+
+    it 'Thor deve ser o primeiro da lista' do
+        hq = AvengersHeadQuarter.new
+
+        hq.put('thor')
+        hq.put('Spiderman')
+        hq.put('hulk')
+
+        expect(hq.list).to start_with('thor')
+    end
+
+    it 'hulk deve ser o último da lista' do
+        hq = AvengersHeadQuarter.new
+
+        hq.put('thor')
+        hq.put('Spiderman')
+        hq.put('hulk')
+
+        expect(hq.list).to end_with('hulk')
+    end
+
+    it 'Deve conter o sobrenome' do
+        avenger = 'Peter Parker'
+
+        expect(avenger).to match(/Parker/)
+        expect(avenger).not_to match(/teste/)
     end
 end

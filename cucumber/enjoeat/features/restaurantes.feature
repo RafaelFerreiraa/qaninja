b363af0f2f11b3c1a310214e8e497a8a81a55b76
Rafael Ferreira <rafael.ferreira@before.com.br>
2019-11-27 13:22:09 -0300

    OK

--- cucumber/enjoeat/features/restaurantes.feature
+++ cucumber/enjoeat/features/restaurantes.feature
@@ -5,26 +5,26 @@ Funcionalidade: Restaurantes
     Sendo um usuário que deseja pedir comida
     Posso acessar a lista de restaurantes
 
-    Contexto: Lista de restaurantes
-        Dado que tenho uma lista de restaurantes
+    Cenario: Restaurantes disponíveis
 
-
-    Cenario: Todos os restaurantes
-
+        Dado que temos os seguintes restaurantes
+            | nome             | categoria    | entrega      | avaliacao |
+            | Bread & Bakery   | Padaria      | 25 minutos   | 4.9       |
+            | Burger House     | Hamburgers   | 30 minutos   | 3.5       |
+            | Coffee Corner    | Cafeteria    | 20 minutos   | 4.8       |
         Quando acesso a lista de restaurantes
-        Então vejo todas as opções disponíveis
-    
-    Cenario: Categorias
+        Então devo ver todos os restaurantes desta lista
 
-        Quando acesso a lista de restaurantes
-        Então cada restaurante deve exibir sua categoria
-    
-    Cenario: Tempo de entrega
+        
+    # @temp
+    # Esquema do Cenario: Restaurantes disponíveis
 
-        Quando acesso a lista de restaurantes
-        Então cada restaurante deve exibir o tempo de entrega
+    #     Quando acesso a lista de restaurantes
+    #     Então cada restaurante deve exibir <id> <nome> <categoria> <entrega> <avaliacao>
 
-    Cenario: Nota de avaliação
+    #     Exemplos:
+    #     | id | nome             | categoria    | entrega      | avaliacao |
+    #     | 0  | "Bread & Bakery" | "Padaria"    | "25 minutos" | 4.9       |
+    #     | 1  | "Burger House"   | "Hamburgers" | "30 minutos" | 3.5       |
+    #     | 2  | "Coffee Corner"  | "Cafeteria"  | "20 minutos" | 4.8       |
 
-        Quando acesso a lista de restaurantes
-        Então cada restaurante deve exibir sua nota de avaliação
--- cucumber/enjoeat/features/step_definitions/restaurantes_steps.rb
+++ cucumber/enjoeat/features/step_definitions/restaurantes_steps.rb
@@ -1,42 +1,61 @@
-Dado("que tenho uma lista de restaurantes") do
-    @restaurante_data = [
-        { nome: 'Bread & Bakery', categoria: 'Padaria', entrega: '25 minutos', avaliacao: 4.9},
-        { nome: 'Burger House', categoria: 'Hamburgers', entrega: '30 minutos', avaliacao: 3.5}
-    ]
+Dado("que temos os seguintes restaurantes") do |table|
+    @restaurante_data = table.hashes
 end
 
 Quando("acesso a lista de restaurantes") do
     visit '/restaurants'
 end
 
-Então("vejo todas as opções disponíveis") do
-    restaurantes = all('.restaurant-item')
-    expect(restaurantes.size).to be > 0
-end
-
-Então("cada restaurante deve exibir sua categoria") do
-
+Então("devo ver todos os restaurantes desta lista") do
     restaurantes = all('.restaurant-item')
 
     @restaurante_data.each_with_index do |value, index|
-        expect(restaurantes[index]).to have_text value[:categoria]
+        expect(restaurantes[index]).to have_text value['nome'].upcase
+        expect(restaurantes[index]).to have_text value['categoria']
+        expect(restaurantes[index]).to have_text value['entrega']
+        expect(restaurantes[index]).to have_text value['avaliacao']
     end
 end
 
-Então("cada restaurante deve exibir o tempo de entrega") do
+  
+# Então("cada restaurante deve exibir {int} {string} {string} {string} {float}") do |id, nome, categoria, entrega, avaliacao|
+#     restaurantes = all('.restaurant-item')
 
-    restaurantes = all('.restaurant-item')
+#     expect(restaurantes[id]).to have_text nome.upcase
+#     expect(restaurantes[id]).to have_text categoria
+#     expect(restaurantes[id]).to have_text entrega
+#     expect(restaurantes[id]).to have_text avaliacao
+# end
 
-    @restaurante_data.each_with_index do |value, index|
-        expect(restaurantes[index]).to have_text value[:entrega]
-    end
-end
-
-Então("cada restaurante deve exibir sua nota de avaliação") do
-
-    restaurantes = all('.restaurant-item')
-
-    @restaurante_data.each_with_index do |value, index|
-        expect(restaurantes[index]).to have_text value[:avaliacao]
-    end
-end
+# 
+# Então("vejo todas as opções disponíveis") do
+    # restaurantes = all('.restaurant-item')
+    # expect(restaurantes.size).to be > 0
+# end
+# 
+# Então("cada restaurante deve exibir sua categoria") do
+# 
+    # restaurantes = all('.restaurant-item')
+# 
+    # @restaurante_data.each_with_index do |value, index|
+        # expect(restaurantes[index]).to have_text value[:categoria]
+    # end
+# end
+# 
+# Então("cada restaurante deve exibir o tempo de entrega") do
+# 
+    # restaurantes = all('.restaurant-item')
+# 
+    # @restaurante_data.each_with_index do |value, index|
+        # expect(restaurantes[index]).to have_text value[:entrega]
+    # end
+# end
+# 
+# Então("cada restaurante deve exibir sua nota de avaliação") do
+# 
+    # restaurantes = all('.restaurant-item')
+# 
+    # @restaurante_data.each_with_index do |value, index|
+        # expect(restaurantes[index]).to have_text value[:avaliacao]
+    # end
+# end

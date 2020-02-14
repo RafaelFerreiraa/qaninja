ad283544fd9413f735782bf770d0a2d7c19fdac1
Rafael Ferreira <rafael.ferreira@before.com.br>
2020-02-14 13:04:52 -0400

    Refatoraçao de Before para Let

--- api/spec/factories.rb
+++ api/spec/factories.rb
@@ -11,6 +11,17 @@ FactoryBot.define do
             Database.new.deleteUser(user.email)
         end
     end
+ 
+    factory :registered_user, class: UserModel do
+        full_name { "Vader Ferreira" }
+        email { "vader@gmail.com" }
+        password { "teste652" }
+
+        after(:build) do |user|
+            Database.new.deleteUser(user.email)
+            ApiUser.save(user.to_hash)
+        end
+    end
 
     factory :user_wrong_email, class: UserModel do
         full_name { "Rafael Ferreira" }
@@ -18,9 +29,36 @@ FactoryBot.define do
         password { "teste652" }
     end
 
-    factory :empty_user, class: UserModel do
+    factory :empty_name_user, class: UserModel do
         full_name { "" }
         email { "sf.rafael20#gmail.com" }
         password { "teste652" }
+    end
+
+    factory :empty_email_user, class: UserModel do
+        full_name { "Rafael Ferreira" }
+        email { "" }
+        password { "teste652" }
+    end
+
+    factory :empty_password_user, class: UserModel do
+        full_name { "Rafael Ferreira" }
+        email { "sf.rafael20#gmail.com" }
+        password { "" }
+    end
+
+    factory :null_name_user, class: UserModel do
+        email { "sf.rafael20@gmail.com" }
+        password { "teste652" }
+    end
+
+    factory :null_email_user, class: UserModel do
+        full_name { "Rafael Ferreira" }
+        password { "teste652" }
+    end
+
+    factory :null_password_user, class: UserModel do
+        full_name { "Rafael Ferreira" }
+        email { "sf.rafael20@gmail.com" }
     end
 end
--- api/spec/post_spec.rb
+++ api/spec/post_spec.rb
@@ -1,32 +1,64 @@
 
 describe "post" do
     context "when new user" do
-        before do
-            @newUser = build(:user).to_hash
-            @resultado = ApiUser.save(@newUser)
-        end
+        let(:resultado) { ApiUser.save(build(:user).to_hash) }
 
-        it { expect(@resultado.response.code).to eql "200" }
+        it { expect(resultado.response.code).to eql "200" }
+    end
+
+    context "when duplicate email" do
+        let(:resultado) { ApiUser.save(build(:registered_user).to_hash) }
+
+        it { expect(resultado.response.code).to eql "409" }
+        it { expect(resultado.parsed_response["msg"]).to eql "Oops. Looks like you already have an account with this email address." }
     end
 
     context "when wrong email" do
-        before do
-            @newUser = build(:user_wrong_email).to_hash
-            @resultado = ApiUser.save(@newUser)
-         end
+        let(:resultado) { ApiUser.save(build(:user_wrong_email).to_hash) }
 
-        it { expect(@resultado.response.code).to eql "412" }
-        it { expect(@resultado.parsed_response["msg"]).to eql "Oops. You entered a wrong email." }
+        it { expect(resultado.response.code).to eql "412" }
+        it { expect(resultado.parsed_response["msg"]).to eql "Oops. You entered a wrong email." }
     end
 
     context "when empty name" do
-        before do
-            @newUser = build(:empty_user).to_hash
-            @resultado = ApiUser.save(@newUser)
-         end
+        let(:resultado) { ApiUser.save(build(:empty_name_user).to_hash) }
 
-        it { expect(@resultado.response.code).to eql "412" }
-        it { expect(@resultado.parsed_response["msg"]).to eql "Validation notEmpty on full_name failed" }
+        it { expect(resultado.response.code).to eql "412" }
+        it { expect(resultado.parsed_response["msg"]).to eql "Validation notEmpty on full_name failed" }
+    end
 
+    context "when empty email" do
+        let(:resultado) { ApiUser.save(build(:empty_email_user).to_hash) }
+
+        it { expect(resultado.response.code).to eql "412" }
+        it { expect(resultado.parsed_response["msg"]).to eql "Validation notEmpty on email failed" }
+    end
+
+    context "when empty password" do
+        let(:resultado) { ApiUser.save(build(:empty_password_user).to_hash) }
+
+        it { expect(resultado.response.code).to eql "412" }
+        it { expect(resultado.parsed_response["msg"]).to eql "Validation notEmpty on password failed" }
+    end
+
+    context "when name is null" do
+        let(:resultado) { ApiUser.save(build(:null_name_user).to_hash) }
+
+        it { expect(resultado.response.code).to eql "412" }
+        it { expect(resultado.parsed_response["msg"]).to eql "Users.full_name cannot be null" }
+    end
+
+    context "when email is null" do
+        let(:resultado) { ApiUser.save(build(:null_email_user).to_hash) }
+
+        it { expect(resultado.response.code).to eql "412" }
+        it { expect(resultado.parsed_response["msg"]).to eql "Users.email cannot be null" }
+    end
+
+    context "when password is null" do
+        let(:resultado) { ApiUser.save(build(:null_password_user).to_hash) }
+
+        it { expect(resultado.response.code).to eql "412" }
+        it { expect(resultado.parsed_response["msg"]).to eql "Users.password cannot be null" }
     end
 end
